ef08ce2d4fd0cc0bad521677b4a212b5dcbfd77b
cherrrypok <66486755+cherrrypok@users.noreply.github.com>
2022-10-10 14:13:52 +0300

    Add files via upload

---
+++ Lab_5/5_N/Wings.py
@@ -0,0 +1,14 @@
+class Wing:
+    def __init__(self, count):
+        self.count = count
+
+    def take_off(self):
+        return "I'm flying!"
+
+
+class OverstuffedWing(Wing):
+    pass
+
+
+class WebbedWing(Wing):
+    pass
